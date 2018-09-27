fdb0c82741390e3f457a2f91f0e2c04b82ca5526
christinechoi <smi77y@gmail.com>
2018-09-26 20:09:17 -0400

    Add presets option to webpack babel loader; fix load file path

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('dist/index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -23,6 +23,8 @@ function createWindow () {
     mainWindow = null
   })
 }
+
+console.log('running main.js')
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
