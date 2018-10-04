ceb6c758f301594a772eeb868f960a31d47cab3f
Joel Pratt <pratt.joel@gmail.com>
2018-10-03 20:40:23 -0400

    Create redux store.

--- src/editor/components/Editor.js
+++ src/editor/components/Editor.js
@@ -1,43 +1,78 @@
 import React from 'react';
 import * as monaco from "monaco-editor";
-const fs = window.require("fs"); 
+const fs = window.require("fs");
 const { ipcRenderer, dialog } = require("electron");
+import { connect } from "react-redux";
+import {
+  addTodo,
+  // monaco,
+  setEditor
+} from "../../js/actions/action"
 
- 
- class Editor extends React.Component {
-   componentDidMount() {
-     self.MonacoEnvironment = {
 
-  getWorkerUrl: function(moduleId, label) {
-    if (label === "json") {
-      return "../dist/json.worker.bundle.js";
-    }
-    if (label === "css") {
-      return "../dist/css.worker.bundle.js";
-    }
-    if (label === "html") {
-      return "../dist/html.worker.bundle.js";
-    }
-    if (label === "typescript" || label === "javascript") {
-      return "../dist/ts.worker.bundle.js";
-    }
-    return "../dist/editor.worker.bundle.js";
+
+class Editor extends React.Component {
+  componentDidMount() {
+    // {this.props.runMonaco}
+    self.MonacoEnvironment = {
+
+      getWorkerUrl: function(moduleId, label) {
+        if (label === "json") {
+          return "../dist/json.worker.bundle.js";
+        }
+        if (label === "css") {
+          return "../dist/css.worker.bundle.js";
+        }
+        if (label === "html") {
+          return "../dist/html.worker.bundle.js";
+        }
+        if (label === "typescript" || label === "javascript") {
+          return "../dist/ts.worker.bundle.js";
+        }
+        return "../dist/editor.worker.bundle.js";
+      }
+    };
+
+    const monacoEditor = monaco.editor.create(document.getElementById("editor-container"), {
+      value: ["function x() {", '\tconsole.log("Whatup world!");', "}"].join("\n"),
+      language: "javascript"
+    });
+    this.props.setEditor(monacoEditor);
   }
-};
-
-  const monacoEditor = monaco.editor.create(document.getElementById("editor-container"), {
-          value: ["function x() {", '\tconsole.log("Whatup world!");', "}"].join("\n"),
-          language: "javascript"
-      });
-
-    console.log('GrandChild did mount.');
-
-  }
-   render() {
+  render() {
+    console.log('editor', this.props.editor);
     return (
-      <div id='editor-container'></div>
+        <div id='editor-container'>
+          <p>{this.props.getEditor}</p>
+          <p>{this.props.todo}</p>
+
+          <p>Test text</p>
+          <button onClick={this.props.toggleTodo}>Click Me</button>
+        </div>
     )
   }
 }
 
-export default Editor;
+function mapStateToProps(state){
+  console.log('the state',state.editorReducer.newEditor);
+    return {
+        getEditor: state.editorReducer.newEditor,
+        todo: state.editorReducer.nextTodoId,
+        editor: state.editorReducer.editor,
+    }
+}
+function mapDispatchToProps (dispatch) {
+  console.log('add todo in mapDispatchToProps', addTodo);
+return  {
+    toggleTodo: () => dispatch(addTodo()),
+    setEditor: (editor) => dispatch(setEditor(editor))
+    // runMonaco: () => dispatch(monaco())
+
+  }
+
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Editor);
+
+// export default Editor;
